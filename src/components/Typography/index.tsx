92037b820c4769fc2e1dd9eb983e04656eb61a59
Guilherme Menecucci <menecucci@hotmail.com>
2022-11-27 14:17:02 -0300

    Typography's Component

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,19 +1,75 @@
-import { ReactNode } from 'react';
-import * as S from './styles';
+import clsx from 'clsx';
+import React, { forwardRef, ReactNode } from 'react';
+import { ThemeColors } from '~@types/_app';
 
-type TypographyProps = {
+import styles from './styles.module.css';
+
+interface ITypographyProps extends React.HTMLAttributes<HTMLHeadingElement> {
   children?: ReactNode;
-  variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-};
+  component: ReactNode;
+  variant: 'display' | 'title' | 'heading' | 'body' | 'label';
+  size: 'large' | 'medium' | 'small';
+  color?: ThemeColors;
+}
 
-const Typography = ({ children, variant }: TypographyProps) => {
-  const TextField = S.TextField[variant];
+const Typography = forwardRef<HTMLHeadingElement, ITypographyProps>(
+  (props: ITypographyProps, ref) => {
+    const {
+      children,
+      variant,
+      component: Component = 'span',
+      size,
+      color,
+      className,
+      ...rest
+    } = props;
+    let textSize = '';
 
-  return (
-      <TextField>
+    switch (variant) {
+      case 'display':
+        if ('large' == size) textSize = 'text-6xl font-normal tracking-[0] leading-[64px]';
+        if ('medium' == size) textSize = 'text-5xl font-normal tracking-[0] leading-[52px]';
+        if ('small' == size) textSize = 'text-4xl font-normal tracking-[0] leading-[44px]';
+        break;
+      case 'heading':
+        if ('large' == size) textSize = 'text-3xl font-normal tracking-[0] leading-10';
+        if ('medium' == size) textSize = 'text-2xl font-normal tracking-[0] leading-9';
+        if ('small' == size) textSize = 'text-xl font-normal tracking-[0] leading-8';
+        break;
+      case 'title':
+        if ('large' == size) textSize = 'text-lg font-normal tracking-[0] leading-7';
+        if ('medium' == size) textSize = 'text-base font-medium tracking-[.15] leading-6';
+        if ('small' == size) textSize = 'text-sm font-medium tracking-[.1] leading-5';
+        break;
+      case 'body':
+        if ('large' == size) textSize = 'text-base font-normal tracking-[.5] leading-6';
+        if ('medium' == size) textSize = 'text-sm font-normal tracking-[.25] leading-5';
+        if ('small' == size) textSize = 'text-xs font-normal tracking-[.4] leading-4';
+        break;
+      case 'label':
+        if ('large' == size) textSize = 'text-sm font-medium tracking-widest leading-5';
+        if ('medium' == size) textSize = 'text-xs font-medium tracking-[.5] leading-4';
+        if ('small' == size) textSize = 'text-[11px] font-medium tracking-[.5] leading-4';
+        break;
+    }
+
+    return (
+      <Component
+        ref={ref}
+        className={clsx(
+          'block align-middle font-sans text-black antialiased dark:text-white',
+          textSize,
+          color && styles[`heading-${color}`],
+          className,
+        )}
+        {...rest}
+      >
         {children}
-      </TextField>
-  );
-};
+      </Component>
+    );
+  },
+);
+
+Typography.displayName = 'Typography';
 
 export default Typography;
