e584d36789c41dfbde609c6d986988baf24f91c3
Guilherme Menecucci <menecucci@hotmail.com>
2022-11-27 13:59:13 -0300

    Configuration Changes

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,8 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
-  `yarn lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+const buildEslintCommand = filenames =>
+  `pnpm lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
 
 module.exports = {
-  'src/**/*.{ts,tsx,css}': [buildEslintCommand],
+  '*.{ts,tsx,css}': [buildEslintCommand],
 };
