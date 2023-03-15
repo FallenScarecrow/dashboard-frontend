0024ea838fd5b165653524d721c07054e5585524
Guilherme Menecucci <menecucci@hotmail.com>
2023-03-15 19:10:20 +0000

    Defining types

--- src/types/pages/_app.d.ts
+++ src/types/pages/_app.d.ts
@@ -1,12 +1,10 @@
-import { NextPage } from 'next';
-import { ReactElement, ReactNode } from 'react';
-
+// export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & {
 export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & {
-  // eslint-disable-next-line no-unused-vars
-  getLayout?: (page: ReactElement) => ReactNode;
+  getLayout: (page: ReactElement | ReactNode) => ReactNode;
 };
 
 export type ThemeColors = 'primary' | 'secondary';
 export type ThemeStatus = 'error' | 'warning' | 'info' | 'success';
 export type TextVariants = 'display' | 'heading' | 'title' | 'body' | 'label';
+// export type Sizes = 'large' | 'medium' | 'small';
 export type Sizes = 'large' | 'medium' | 'small';
---
+++ src/types/pages/dashboard/index.d.ts
@@ -0,0 +1,5 @@
+import { IMovies } from '~@types/_api';
+
+export interface IDashboardProps {
+  movies: IMovies;
+}
