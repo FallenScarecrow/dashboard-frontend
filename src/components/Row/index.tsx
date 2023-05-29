6013a5dace9c818031b2a9766d7a3b8404063a04
Guilherme Menecucci <menecucci@hotmail.com>
2023-05-29 15:52:19 +0100

    Creating Row Component

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,10 +1,23 @@
 import clsx from 'clsx';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import { TRowProps } from '~@types/components/Row';
 
-const Row = ({ children, className = '' }: TRowProps) => {
-  return <section className={clsx('grid grid-cols-12', className)}>{children}</section>;
-};
+const Row = forwardRef<HTMLElement, TRowProps>(
+  ({ children, flex = false, className = '', style, ...props }, ref) => {
+    return (
+      <section
+        ref={ref}
+        className={clsx(!flex && 'grid grid-cols-12', className)}
+        style={style}
+        {...props}
+      >
+        {children}
+      </section>
+    );
+  },
+);
+
+Row.displayName = 'Row';
 
 export default Row;
