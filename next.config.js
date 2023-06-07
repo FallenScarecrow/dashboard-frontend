aed2e6158a0813498b59442be97a76cb619cc56e
Guilherme Menecucci <menecucci@hotmail.com>
2023-06-07 21:12:53 +0100

    Configuration

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@
 // const withTM = require('next-transpile-modules');
 
 const nextConfig = {
-  strictMode: false,
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
@@ -34,58 +33,6 @@ const nextConfig = {
         port: '3001',
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        // Force disable caching for any NextAuth api routes. We need to do this because by default
-        // these API endpoints do not return a Cache-Control header. If the header is missing, FrontDoor
-        // CDN **will** cache the pages, which is a security risk and can return the wrong user:
-        // https://docs.microsoft.com/en-us/azure/frontdoor/front-door-caching#cache-expiration
-        source: '/api/auth/:slug',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0',
-          },
-        ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      // if the header `x-authorized` is present and
-      // contains a matching value, this rewrite will be applied
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-authorized',
-            value: '(?<authorized>yes|true)',
-          },
-        ],
-        destination: '/home?authorized=:authorized',
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      // if the header `x-authorized` is present and
-      // contains a matching value, this redirect will be applied
-      {
-        source: '/',
-        has: [
-          {
-            type: 'header',
-            key: 'x-authorized',
-            value: '(?<authorized>yes|true)',
-          },
-        ],
-        permanent: false,
-        destination: '/home?authorized=:authorized',
-      },
-    ];
   },
 };
 
